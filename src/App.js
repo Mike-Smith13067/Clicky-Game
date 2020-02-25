aeb79be9c8d9f73b12a16a1a70c5ee6a26eae0ac
Mike <hockeydad66@live.com>
2020-02-25 12:12:55 -0700

    components completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Wrapper from "./components/Wrapper";
+import ImageCard from "./components/ImageCard";
+import cardImage from "./ cardImage.json";
+import "./index.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var correctPick = 0;
+var bestScore = 0;
+var notification = "Select a card below to reveal a SEC team logo. Keep picking cards until you reveal the original logo";
+
+class App extends Component {
+  state = {
+    cardImage,
+    correctPick,
+    bestScore,
+    notification
+
+  };
+
+  randomImages = cardImage => {
+    cardImage.sort((a, b) => {
+      return 0.5 - Math.random();
+    });
+  };
+  // Picking the original logo
+  updatePicked = id => {
+    const cardImage = this.state.cardImage;
+    const pickedImage = cardImage.filter(cardImage => cardImage.id === id);
+
+    if (pickedImage[0].clicked) {
+      correctPick = 0;
+      notification = "Opps!! You already clicked that one. Try again?"
+      this.randomImages(cardImage);
+
+      cardImage.forEaach(image => cardImage.clicked = false);
+
+      this.setState({ notification });
+      this.setState({ correctPick });
+      this.setState({ cardImage });
+    }
+
+    // Picking anycard other than original logo
+
+    else if (correctPick < 11) {
+      pickedImage[0].clicked = true;
+      correctPick++;
+
+      notification = "Nice choice- keep going!!";
+
+      if (correctPick > bestScore) {
+        bestScore = correctPick;
+        this.setState({ bestScore });
+      }
+
+      this.randomImages(cardImage);
+      this.setState({ notification });
+      this.setState({ correctPick });
+      this.setState({ cardImage });
+    }
+
+    // Perfect game
+
+    else {
+      pickedImage[0] = true;
+      correctPick = 0
+      notification = "Well Done!! Perfect Game!!";
+      bestScore = 12;
+      this.setState({ bestScore });
+    }
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Card Clicky Game (SEC version)</Title>
+        <h2 className="headerNotification">{this.state.notification}</h2>
+        <div className="container score">
+          <h3>Correct Picks: {this.state.correctPicks}
+            <br /> Best Score: {this.state.bestScore}
+          </h3>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            this.state.cardImage.map(inage => (
+          <ImageCard
+              updatePicked={this.updatePicked}
+              id={image.id}
+              key={image.id}
+              image={image.image}
+            />
+            ))}
+      </div>
+        </div>
+      </Wrapper>
+    );
+  }
 }
 
-export default App;
+  export default App;
---
+++ src/components/ImageCard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ImageCard.css";
+
+const ImageCard = props => (
+    <div onClick={() => props.updateClicked(props.id)} className="col-md-3 logo-image zoom">
+        <div className="img-container">
+            <img alt={props.name} src={props.image} />
+        </div>
+    </div>
+)
+
+export default ImageCard;
