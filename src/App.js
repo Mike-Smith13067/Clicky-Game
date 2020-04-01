2bd7d10b7ee1b1c80f6e7a5e0ee3664291e74016
Mike <hockeydad66@live.com>
2020-04-01 05:51:07 -0600

    removed missstate

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ console.log (cardImage);
 
     //Correct Pick
 
-    } else if (correctPick < 12) {
+    } else if (correctPick < 11) {
       pickedImage[0].clicked = true;
       correctPick++;
 
@@ -62,7 +62,7 @@ console.log (cardImage);
       pickedImage[0] = true;
       correctPick = 0
       notification = "Well Done!! Perfect Game!!";
-      bestScore = 13;
+      bestScore = 12;
       this.setState({ bestScore });
 
       cardImage.map(cardImage => cardImage.clicked = false);
